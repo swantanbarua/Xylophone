3a8d5787d92842f0d8bc2dfae0a2a6c44e058f71
Swantan Barua <work.swantan@gmail.com>
2022-06-25 16:22:57 +0530

    View Controller linked to storyboard

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    var timer = Timer()
+    
+    @IBAction func xylophoneButtonPressed(_ sender: UIButton) {
+        
+        player = try! AVAudioPlayer(contentsOf: Bundle.main.url(forResource: String(sender.currentTitle!), withExtension: "wav")!)
+        player.play()
+    }
 }
 
