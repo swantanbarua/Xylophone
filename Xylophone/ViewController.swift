2886680196d84a832de494c608f8a128eeeaff04
Swantan Barua <work.swantan@gmail.com>
2022-06-26 13:02:44 +0530

    button opacity changing for sometime after pressed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -11,14 +11,19 @@ import AVFoundation
 class ViewController: UIViewController {
 
     var player: AVAudioPlayer!
+    var timer: Timer?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func xylophoneButtonPressed(_ sender: UIButton) {
-
+        
         player = try! AVAudioPlayer(contentsOf: Bundle.main.url(forResource: String(sender.currentTitle!), withExtension: "wav")!)
         player.play()
+        sender.alpha = 0.7
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            sender.alpha = 1.0
+        }
     }
 }
